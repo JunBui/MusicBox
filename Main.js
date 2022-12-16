c6484077b94060508ae46bb67e58fdf9baf65ae9
LAPTOP-RSH17G7M\LENOVO <quanghuy180623@gmail.com>
2022-12-16 14:41:33 +0700

    add javascript play

--- Main.js
+++ Main.js
@@ -1,199 +1,149 @@
-let pop_song_left = document.getElementById('pop_song_left');
-let pop_song_right = document.getElementById('pop_song_right');
-let pop_song = document.getElementsByClassName('pop_song')[0];
+const music = new Audio('./assets/css/audio/1.mp3');
+// music.play();
 
-pop_song_right.addEventListener('click', () => {
-    pop_song.scrollLeft += 330;
+const songs = [
+    {
+        id: '1',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/1.jpg",
+    },
+    {
+        id: '2',
+        SongName: `Như em đã thấy<br> <div class="sub_title">Nân</div>`,
+        poster: "./assets/css/fonts/img/2.jpg",
+    },
+    {
+        id: '3',
+        SongName: `Lạ lùng <br> <div class="sub_title">Vũ</div>`,
+        poster: "./assets/css/fonts/img/3.jpg.jpg",
+    },
+    {
+        id: '4',
+        SongName: `Nụ cười nàng<br> <div class="sub_title">Pjnboys</div>`,
+        poster: "./assets/css/fonts/img/4.jpg",
+    },
+    {
+        id: '5',
+        SongName: `Một ngàn nỗi đau<br>
+        <div class="sub_title">Văn Mai Hương</div>`,
+        poster: "./assets/css/fonts/5.jpg",
+    },
+    {
+        id: '6',
+        SongName: `Vì anh đâu có biết<br>
+        <div class="sub_title">Madihu,Vũ</div>`,
+        poster: "./assets/css/fonts/img/6.jpg",
+    },
+    {
+        id: '7',
+        SongName: `Anh yêu em cực<br>
+        <div class="sub_title">Linh Thộn, Minh Vũ</div>`,
+        poster: "./assets/css/fonts/img/7.jpg",
+    },
+    {
+        id: '8',
+        SongName: `Ta còn một đời để yêu<br>
+        <div class="sub_title">Đoàn Minh Quân</div>`,
+        poster: "./assets/css/fonts/img/8.jpg",
+    },
+    {
+        id: '9',
+        SongName: `Đế vương<br>
+        <div class="sub_title">Đình Dũng`,
+        poster: "./assets/css/fonts/img/9.jpg",
+    },
+    {
+        id: '10',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/10.jpg",
+    },
+    {
+        id: '11',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/11.jpg",
+    },
+    {
+        id: '12',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/12.jpg",
+    },
+    {
+        id: '13',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/13.jpg",
+    },
+    {
+        id: '14',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/14.jpg",
+    },
+    {
+        id: '15',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/15.jpg",
+    },
+    {
+        id: '16',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/16.jpg",
+    },
+    {
+        id: '17',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/17.jpg",
+    },
+    {
+        id: '18',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/18.jpg",
+    },
+    {
+        id: '19',
+        SongName: `Tình yêu là <br> <div class="sub_title">Hiền Hồ</div>`,
+        poster: "./assets/css/fonts/img/19.jpg",
+    },
+    {
+        id: '20',
+        SongName: `Chơi<br> <div class="sub_title">Wack</div>`,
+        poster: "./assets/css/fonts/img/20.jpg",
+    }
+]
+
+Array.from(document.getElementsByClassName('.song_item')).forEach((e, i) =>{
+    e.getElementsByTagName('img')[0].src = songs[i].poster;
+    e.getElementsByTagName('h5')[0].innerHTML= songs[i].SongName;
+})
+
+let masterPlay = document.getElementById('masterPlay');
+let wave = document.getElementById('wave');
+
+
+masterPlay.addEventListener('click', () =>{
+    if(music.paused || music.currentTime <= 0){
+        music.play();
+        wave.classList.add('active1');
+        masterPlay.classList.remove('bi-play-fill')
+        masterPlay.classList.add('bi-pause-fill')
+    }else{
+        music.pause();
+        wave.classList.remove('active1');
+        masterPlay.classList.add('bi-play-fill')
+        masterPlay.classList.remove('bi-pause-fill')   
+    }
 });
 
-pop_song_lefy.addEventListener('click', () => {
-    pop_song.scrollLeft -= 330;
-});
+let index = 0;
+let poster_master_play = document.getElementById('poster_master_play');
 
-function Validator(options) {
-    function getParent(element, selector) {
-        while (element.parentElement) {
-            if (element.parentElement.matches(selector)) {
-                return element.parentElement;
-            }
-            element = element.parentElement;
-        }
-    }
-
-    var selectorRules = {};
-
-    // Hàm thực hiện validate
-    function validate(inputElement, rule) {
-        var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
-        var errorMessage;
-
-        // Lấy ra các rules của selector
-        var rules = selectorRules[rule.selector];
-        
-        // Lặp qua từng rule & kiểm tra
-        // Nếu có lỗi thì dừng việc kiểm
-        for (var i = 0; i < rules.length; ++i) {
-            switch (inputElement.type) {
-                case 'radio':
-                case 'checkbox':
-                    errorMessage = rules[i](
-                        formElement.querySelector(rule.selector + ':checked')
-                    );
-                    break;
-                default:
-                    errorMessage = rules[i](inputElement.value);
-            }
-            if (errorMessage) break;
-        }
-        
-        if (errorMessage) {
-            errorElement.innerText = errorMessage;
-            getParent(inputElement, options.formGroupSelector).classList.add('invalid');
-        } else {
-            errorElement.innerText = '';
-            getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
-        }
-
-        return !errorMessage;
-    }
-
-    // Lấy element của form cần validate
-    var formElement = document.querySelector(options.form);
-    if (formElement) {
-        // Khi submit form
-        formElement.onsubmit = function (e) {
-            e.preventDefault();
-
-            var isFormValid = true;
-
-            // Lặp qua từng rules và validate
-            options.rules.forEach(function (rule) {
-                var inputElement = formElement.querySelector(rule.selector);
-                var isValid = validate(inputElement, rule);
-                if (!isValid) {
-                    isFormValid = false;
-                }
-            });
-
-            if (isFormValid) {
-                // Trường hợp submit với javascript
-                if (typeof options.onSubmit === 'function') {
-                    var enableInputs = formElement.querySelectorAll('[name]');
-                    var formValues = Array.from(enableInputs).reduce(function (values, input) {
-                        
-                        switch(input.type) {
-                            case 'radio':
-                                values[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked').value;
-                                break;
-                            case 'checkbox':
-                                if (!input.matches(':checked')) {
-                                    values[input.name] = '';
-                                    return values;
-                                }
-                                if (!Array.isArray(values[input.name])) {
-                                    values[input.name] = [];
-                                }
-                                values[input.name].push(input.value);
-                                break;
-                            case 'file':
-                                values[input.name] = input.files;
-                                break;
-                            default:
-                                values[input.name] = input.value;
-                        }
-
-                        return values;
-                    }, {});
-                    options.onSubmit(formValues);
-                }
-                // Trường hợp submit với hành vi mặc định
-                else {
-                    formElement.submit();
-                }
-            }
-        }
-
-        // Lặp qua mỗi rule và xử lý (lắng nghe sự kiện blur, input, ...)
-        options.rules.forEach(function (rule) {
-
-            // Lưu lại các rules cho mỗi input
-            if (Array.isArray(selectorRules[rule.selector])) {
-                selectorRules[rule.selector].push(rule.test);
-            } else {
-                selectorRules[rule.selector] = [rule.test];
-            }
-
-            var inputElements = formElement.querySelectorAll(rule.selector);
-
-            Array.from(inputElements).forEach(function (inputElement) {
-               // Xử lý trường hợp blur khỏi input
-                inputElement.onblur = function () {
-                    validate(inputElement, rule);
-                }
-
-                // Xử lý mỗi khi người dùng nhập vào input
-                inputElement.oninput = function () {
-                    var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
-                    errorElement.innerText = '';
-                    getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
-                } 
-            });
-        });
-    }
-
-}
-
-
-
-// Định nghĩa rules
-// Nguyên tắc của các rules:
-// 1. Khi có lỗi => Trả ra message lỗi
-// 2. Khi hợp lệ => Không trả ra cái gì cả (undefined)
-Validator.isRequired = function (selector, message) {
-    return {
-        selector: selector,
-        test: function (value) {
-            return value ? undefined :  message || 'Vui lòng nhập trường này'
-        }
-    };
-}
-
-Validator.isEmail = function (selector, message) {
-    return {
-        selector: selector,
-        test: function (value) {
-            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return regex.test(value) ? undefined :  message || 'Trường này phải là email';
-        }
-    };
-}
-
-Validator.minLength = function (selector, min, message) {
-    return {
-        selector: selector,
-        test: function (value) {
-            return value.length >= min ? undefined :  message || Vui lòng nhập tối thiểu ${min} kí tự;
-        }
-    };
-}
-
-Validator.isConfirmed = function (selector, getConfirmValue, message) {
-    return {
-        selector: selector,
-        test: function (value) {
-            return value === getConfirmValue() ? undefined : message || 'Giá trị nhập vào không chính xác';
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
+Array.from(document.getElementsByClassName('.playListPlay')).forEach((e)=>{
+    e.addEventListener('click', (el) => {
+        index = el.target.id;
+        // console.log(index);
+        music.src = `.assets/css/audio/${index}.mp3`;
+        poster_master_play.src = `./assets/css/fonts/img/${index}.jpg`
+        music.play();
+    })
+})
 
 
 
